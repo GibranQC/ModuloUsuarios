53aee874fe42287ec266e1e6c7995eb626a98bbf
GibranQC <gibranqc9131@gmail.com>
2021-07-12 15:46:03 -0500

    d

--- Back-end/modules/extras/extra.ctrl.js
+++ Back-end/modules/extras/extra.ctrl.js
@@ -3,37 +3,10 @@ var usersCtrl = require('../users/users.lib')
 var hospitalCtrl = require('../extras/hospitals/hospitals.lib');
 var async = require('async')
 
-module.exports.listInstitutions = listInstitutions;
-module.exports.listSizes = listSizes;
-module.exports.listCounters = listCounters;
-module.exports.listCies10 = listCies10;
-module.exports.listCies9 = listCies9;
-module.exports.listTypesProcedures = listTypesProcedures;
-module.exports.listProjects = listProjects;
-module.exports.listUnitsMeasurement = listUnitsMeasurement;
-module.exports.listAssortments = listAssortments;
-module.exports.listPresentations = listPresentations;
-module.exports.listLotsBySupplie = listLotsBySupplie;
-module.exports.listLots = listLots;
-module.exports.listSupplieType = listSupplieType;
-module.exports.listGenericCatalog = listGenericCatalog;
-module.exports.getStockLotsBySupplies = getStockLotsBySupplies;
-module.exports.listIncidences = listIncidences;
 module.exports.listUsers = listUsers;
-module.exports.listSupplies = listSupplies;
-module.exports.listLotsBySupply = listLotsBySupply;
-module.exports.listDoctors = listDoctors;
-module.exports.listHospitals = listHospitals;
+
 module.exports.listProfiles = listProfiles;
-module.exports.listRoomQX = listRoomQX;
-module.exports.listSpecialities = listSpecialities;
-module.exports.updateRooms = updateRooms;
-module.exports.deleteRooms = deleteRooms;
-module.exports.listPeriods = listPeriods;
-module.exports.listProviders =listProviders;
-module.exports.ListTypeOutles = ListTypeOutles;
-module.exports.ListPriorities = ListPriorities;
-module.exports.listDays = listDays;
+
 
 function listUsers(req, res, next) {
   let data = {}
